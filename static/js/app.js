aa2e9f04c75520e046cd4a4bd2e6890123f035b0
Luan fonseca de farias <luanfonceca@gmail.com>
2017-07-26 23:07:58 -0300

    Update site

--- static/js/app.js
+++ static/js/app.js
@@ -25,3 +25,51 @@ $('.carousel').carousel({
   interval: 6000,
   pause: "false"
 });
+
+var map;
+var infoWindow;
+var service;
+
+function initMap() {
+  var position = new google.maps.LatLng(-5.817595, -35.214317);
+  map = new google.maps.Map($('#map')[0], {
+    zoom: 15,
+    scrollwheel: false,
+    center: position
+  });
+
+  google.maps.event.addDomListener(window, 'resize', function() {
+    map.setCenter(position);
+  });
+
+  infoWindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.getDetails({
+    placeId: 'ChIJyUasBfb_sgcRYvsewdiOxm4'
+  }, function(place, status) {
+    if (status != google.maps.places.PlacesServiceStatus.OK) {
+      alert(status);
+      return;
+    }
+
+    var marker = new google.maps.Marker({
+      map: map,
+      title: place.name,
+      position: place.geometry.location,
+      icon: {
+        url: 'static/img/map-marker.svg',
+        scaledSize: new google.maps.Size(25, 35)
+      },
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.setContent(
+        '<strong>' + place.name + '</strong>' +
+        "<br>" + place.formatted_address
+      );
+      infoWindow.open(map, marker);
+    });
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
