8ebb01cad7c578088ad482c9245b208495131da6
Luan fonseca de farias <luanfonceca@gmail.com>
2017-08-04 00:05:40 -0300

    Improve performance, minify images and add scrol to menu

--- static/js/app.js
+++ static/js/app.js
@@ -144,3 +144,33 @@ if (width >= 768) {
     }
   });
 }
+
+$('a[href*="#"]').not('[href="#"]').click(function(event) {
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    var target = $(this.hash);
+    var navbarHeight = 90;
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    
+    if (!target.length) {
+      return
+    }
+
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: target.offset().top - navbarHeight
+    }, 1000, function() {
+      var $target = $(target);
+      $target.focus();
+      if ($target.is(":focus")) {
+        return false;
+      } else {
+        $target.attr('tabindex','-1');
+        $target.focus();
+      };
+    });
+  }
+});
